ff3b9a6015eabbb5a3e95cc8a2db6890a197f5cc
Umar Khursheed <137692710+umarsayed12@users.noreply.github.com>
2025-03-29 13:01:11 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
         "/api": {
-          target: import.meta.env.VITE_API_URL, // ✅ Ensure this is correct
+          target: "https://codelab-sq6v.onrender.com", // ✅ Ensure this is correct
           changeOrigin: true,
           secure: false,
           ws:true,
